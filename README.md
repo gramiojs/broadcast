9ec86bb4e6f71554792c68103d0f29cf49b0af81
kravetsone <57632712+kravetsone@users.noreply.github.com>
2024-12-04 22:46:56 +0300

    chore: WIP design

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 [![npm](https://img.shields.io/npm/v/@gramio/auto-answer-callback-query?logo=npm&style=flat&labelColor=000&color=3b82f6)](https://www.npmjs.org/package/@gramio/auto-answer-callback-query)
 [![JSR](https://jsr.io/badges/@gramio/auto-answer-callback-query)](https://jsr.io/@gramio/auto-answer-callback-query)
 [![JSR Score](https://jsr.io/badges/@gramio/auto-answer-callback-query/score)](https://jsr.io/@gramio/auto-answer-callback-query)
+
+```ts
+const redis = new Redis();
+
+const defineJob = initJobify(redis);
+
+const bot = new Bot(process.env.BOT_TOKEN as string);
+
+const broadcast = new Broadcast(defineJob("broadcast"))
+    .type("test", (a: number) => a + 1)
+    .type("test2", (b: string) => `${b}a`);
+
+broadcast.start("test", [[1]]);
+```
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,51 @@ import type { initJobify } from "jobify";
 
 // TODO: export it
 type Job = ReturnType<ReturnType<typeof initJobify>>;
+type IsNever<T> = [T] extends [never] ? true : false;
 
-export class Broadcast {
+// TODO: replace it
+type Action = Function;
+
+export class Broadcast<
+	Types extends Record<
+		string,
+		{
+			action: Function;
+		}
+	> = {},
+> {
 	job: Job;
+
+	types: {
+		name: keyof Types;
+		action: Action;
+	}[] = [];
 
 	constructor(job: Job) {
 		this.job = job;
 	}
+
+	type<const T extends string, F extends Function>(
+		type: T,
+		action: F,
+	): Broadcast<
+		Types & {
+			[K in T]: {
+				action: F;
+			};
+		}
+	> {
+		this.types.push({
+			name: type,
+			action,
+		});
+
+		return this;
+	}
+
+	start<Type extends keyof Types>(
+		name: Type,
+		// a: Types[Type]["action"],
+		args: Parameters<Types[Type]["action"]>[],
+	) {}
 }
